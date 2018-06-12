c633c3f779148ff0c5545a7e3c6ee34ad7f36250
Alex-Lakin <alex.lakin@hotmail.co.uk>
2018-06-12 14:45:39 +0100

    drop down populated from api

--- src/containers/CharactersContainer.js
+++ src/containers/CharactersContainer.js
@@ -6,14 +6,35 @@ class CharactersContainer extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      characters: [],
+      currentCharacter : null
+    };
+
+    this.handleCharacterSelected = this.handleCharacterSelected.bind(this);
+  }
+
+  componentDidMount(){
+    const url = 'http://hp-api.herokuapp.com/api/characters';
+    fetch(url)
+      .then(res => res.json())
+      .then(characterData => this.setState({characters: characterData}))
+      .catch(error => console.log("Error", error))
+  };
+
+  handleCharacterSelected(index){
+    const selectedCharacter = this.state.characters[index]
+    this.setState( {currentCharacter: selectedCharacter} )
   }
 
   render(){
     return (
       <div>
         <h1>Character Container</h1>
-        <CharacterSelector />
+        <CharacterSelector
+          characters = {this.state.characters}
+          onCharacterSelected = {this.handleCharacterSelected}
+        />
         <CharacterDetail />
       </div>
     );
